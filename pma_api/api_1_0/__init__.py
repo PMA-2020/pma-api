e1a9e3d19310ba8343b5e28a9b76021a7327faa8
joeflack4 <joeflack4@gmail.com>
2017-08-15 13:14:12 -0400

    Fix in data endpoint.

--- pma_api/api_1_0/__init__.py
+++ pma_api/api_1_0/__init__.py
@@ -76,8 +76,8 @@ def get_characteristic(code):
 
 @api.route('/data')
 def get_data():
-    # all_data = data_refined_query(request.args)  # TODO: Put this back.
-    all_data = Data.query.all()
+    all_data = data_refined_query(request.args)
+    # all_data = Data.query.all()
     return jsonify(json_obj = {
         'resultsSize': len(all_data),
         'results': [d.full_json() for d in all_data]
