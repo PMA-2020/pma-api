0a2d6d73dcf1aedd927c070480082bb1444b2d7b
joeflack4 <joeflack4@gmail.com>
2017-08-17 13:25:17 -0400

    Added TestRoutes class and scaffolding for TestDB class, which is currently commented out as WIP.

--- test/test.py
+++ test/test.py
@@ -1,20 +1,74 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Unit tests for PPP package."""
+"""Unit tests."""
+import os
 import unittest
 
+from manage import app
 
-class TestApiRoutes(unittest.TestCase):
-    """Unit tests for the Resource class."""
 
-    def test_get_resources(self):
-        """Test initialization."""
-        # from pma_api.api_1_0 import get_resources
-        pass
+class TestRoutes(unittest.TestCase):
+    """Test routes."""
+
+    ignore_routes = ('/static/<path:filename>',)
+    ignore_end_patterns = ('>',)
+
+    def setUp(self):
+        """Set up: Put Flask app in test mode."""
+        app.testing = True
+        self.app = app.test_client()
+
+    @staticmethod
+    def valid_route(route):
+        """Validate route.
+
+        Args:
+            route (str): Route url pattern.
+
+        Returns:
+            bool: True if valid, else False.
+        """
+        if route in TestRoutes.ignore_routes \
+                or route.endswith(TestRoutes.ignore_end_patterns):
+            return False
+        return True
+
+    def test_routes(self):
+        """Smoke test routes to ensure no runtime errors.."""
+        routes = [route.rule for route in app.url_map.iter_rules()
+                  if self.valid_route(route.rule)]
+        for route in routes:
+            self.app.get(route)
+
+
+# class TestDB(unittest.TestCase):  # TODO: Adapt from tutorial.
+#     """Test database functionality.
+#
+#     Tutorial: http://flask.pocoo.org/docs/0.12/testing/
+#     """
+#
+#     def setUp(self):
+#         """Set up: (1) Put Flask app in test mode, (2) Create temp DB."""
+#         import tempfile
+#         from manage import initdb
+#         self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+#         app.testing = True
+#         self.app = app.test_client()
+#         with app.app_context():
+#             initdb()
+#
+#     def tearDown(self):
+#         """Tear down: (1) Close temp DB."""
+#         os.close(self.db_fd)
+#         os.unlink(app.config['DATABASE'])
+#
+#     def test_empty_db(self):
+#         """Test empty database."""
+#         resp = self.app.get('/')
+#         assert b'No entries here so far' in resp.data
 
 
 if __name__ == '__main__':
-    import os
     from test.utils.doctest_unittest_runner import doctest_unittest_runner
     TEST_DIR = os.path.dirname(os.path.realpath(__file__)) + '/'
     doctest_unittest_runner(test_dir=TEST_DIR, relative_path_to_root='../',
