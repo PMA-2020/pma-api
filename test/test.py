5c89b75e0606833605b3f5492ebb53c3a9e63c07
joeflack4 <joeflack4@gmail.com>
2017-08-17 10:51:30 -0400

    Refactored unittest entrypoint by extracting logic for running unittests and doctests together and placing that in utils/doctest_unittest_runner.

--- test/test.py
+++ test/test.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Unit tests for PPP package."""
-import doctest
-import unittest
-from argparse import ArgumentParser
 import os
+import unittest
 
-
-SRC_PKG_NAME, TEST_PKG_NAME = 'pma_api', 'test'
-PKG_NAMES = [SRC_PKG_NAME, TEST_PKG_NAME]
-TEST_DIR = os.path.dirname(os.path.realpath(__file__)) + '/'
+from test.utils.doctest_unittest_runner import doctest_unittest_runner
 
 
 class PlaceholderTestClass(unittest.TestCase):
@@ -21,69 +16,6 @@ class PlaceholderTestClass(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    def get_args():
-        """CLI for test runner."""
-        desc = 'Run tests for package.'
-        parser = ArgumentParser(description=desc)
-        doctests_only_help = 'Specifies whether to run doctests only, as ' \
-                             'opposed to doctests with unittests. Default is' \
-                             ' False.'
-        parser.add_argument('-d', '--doctests-only', action='store_true',
-                            help=doctests_only_help)
-        args = parser.parse_args()
-        return args
-
-    def get_test_modules(test_package):
-        """Get files to test.
-
-        Args:
-            test_package (str): The package containing modules to test.
-
-        Returns:
-            list: List of all python modules in package.
-
-        """
-        # TODO: Make dynamic. Maybe make TEST_PACKAGES a dict (mod name + path)
-        if test_package == SRC_PKG_NAME:
-            root_dir = TEST_DIR+'../'+SRC_PKG_NAME
-        elif test_package == TEST_PKG_NAME:
-            root_dir = TEST_DIR
-        else:
-            raise Exception('Test package \'{}\' not found.'
-                            .format(test_package))
-
-        test_modules = []
-        for dummy, dummy, filenames in os.walk(root_dir):
-            for file in filenames:
-                if file.endswith('.py'):
-                    file = file[:-3]
-                    test_module = test_package + '.' + file
-                    test_modules.append(test_module)
-        return test_modules
-
-    def get_test_suite(test_packages):
-        """Get suite to test.
-
-        Args:
-            test_packages (list): List of strings of package names.
-
-        Returns:
-            TestSuite: Suite to test.
-        """
-        suite = unittest.TestSuite()
-        for package in test_packages:
-            pkg_modules = get_test_modules(test_package=package)
-            for pkg_module in pkg_modules:
-                suite.addTest(doctest.DocTestSuite(pkg_module))
-        return suite
-
-    PARAMS = get_args()
-    TEST_SUITE = get_test_suite(PKG_NAMES)
-    unittest.TextTestRunner(verbosity=1).run(TEST_SUITE)
-    if PARAMS.doctests_only:  # TODO: For dev testing needs. Refactor.
-        pass
-        # TEST_SUITE = get_test_suite()
-        # unittest.TextTestRunner(verbosity=1).run(TEST_SUITE)
-    else:
-        # unittest.main()
-        pass
+    test_dir = os.path.dirname(os.path.realpath(__file__)) + '/'
+    doctest_unittest_runner(test_dir=test_dir, relative_path_to_root='../',
+                            package_names=['pma_api', 'test'])
