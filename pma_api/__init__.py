04ff560c7a4fde9ef59bf254ace17bc5f8068125
joeflack4 <joeflack4@gmail.com>
2017-09-06 13:10:22 -0400

    Added dependency flask_cors and set CORS to * using default behavior of flask_cors API.

--- pma_api/__init__.py
+++ pma_api/__init__.py
@@ -1,6 +1,7 @@
 """Definition of application object."""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 from config import config
 
@@ -20,6 +21,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
+    CORS(app)
     db.init_app(app)
 
     from .api_1_0 import api as api_1_0_blueprint
