9fa4d1cb220b4475c983d88324ec71c38da5ad0f
joeflack4 <joeflack4@gmail.com>
2018-09-04 12:58:12 -0400

    Made some updates to the admin portal feature. - Removed unused imports. - Updated docstring/comments.

--- pma_api/api_1_0/admin.py
+++ pma_api/api_1_0/admin.py
@@ -1,35 +1,31 @@
-"""Routes related to the datalab."""
-from flask import request, render_template
+"""Routes related specifically to the admin portal."""
+from flask import render_template
 
 from . import api
-from . import caching
-from ..models import Cache
-from ..response import ApiResult, QuerySetApiResult
-from ..queries import DatalabData
 
 
 @api.route('/admin')
 def admin_route():
-    """Admin route
+    """Route to admin portal for uploading and managing datasets.
 
-    .. :quickref: admin; stuff 
+    .. :quickref: admin; Route to admin portal for uploading and managing
+    datasets.
 
-    Args:
+    Args: n/a
 
-
-    Query Args:
-
+    Query Args: n/a
 
     Returns:
+        flask.render_template(): A rendered HTML template.
 
+    Examples: n/a
 
     Details:
-
-
-    Example:
-
+        For more information on the features that will be added to this route,
+        take a look at: https://github.com/PMA-2020/pma-api/issues/32
     """
 
-    """return QuerySetApiResult(json_obj, 'json', queryInput=query_input,
-                             chartOptions=chart_options)"""
+    # - Temporarily here just for reference -jef, 2018/09/04
+    # return QuerySetApiResult(json_obj, 'json', queryInput=query_input,
+    #                          chartOptions=chart_options)
     return render_template('index.html')
