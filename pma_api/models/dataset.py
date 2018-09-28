f3cd6d28064bccee01884eae2098c62d1cf445ca
Richard Nguyen <richardnguyen5@richards-mbp.sph.ad.jhsph.edu>
2018-09-28 10:08:49 -0400

    changes to dataset class

--- pma_api/models/dataset.py
+++ pma_api/models/dataset.py
@@ -1,49 +1,57 @@
+import os
+
+
 from datetime import datetime
 from hashlib import md5
+
+
 
 from . import db
 
 
 class Dataset(db.Model):
     """
-
     datasetType: string / varchar; factor var of ('data', 'metadata', 'full')
     datasetSubType: string / varchar ~256?; domain of, ('CCRR', 'METADATA_CLASS', 'all'/'full')
 
     Example usage:
            new_dataset = Dataset(file_path)
-
     """
-
     __tablename__ = 'dataset'
     ID = db.Column(db.Integer, primary_key=True)
-    data = db.BLOB
+    data = db.Column(db.LargeBinary)
     datasetDisplayName = db.Column(db.String, nullable=False)
     uploadDate = db.Column(db.String, nullable=False)
-    versionNumber = db.Column(db.String, nullable=False, unique = True)
+    versionNumber = db.Column(db.String, nullable=False, unique=True)
     datasetType = db.Column(db.String, nullable=False)
-    datasetSubType = db.Column(db.String, nullable=False)
     isActiveStaging = db.Column(db.Boolean, nullable=False)
     isActiveProduction = db.Column(db.Boolean, nullable=False)
 
     def __init__(self, file_path):
+        """Initialize instance of dataset"""
+        data = open(file_path, 'r')
+        data2 = data.read()
 
+        datasetDisplayName = os.path.basename(file_path)
+        naming = datasetDisplayName.split('-')
+        uploadDate = naming[1]
+        versionNumber = naming[2]
+        datasetType = 'full'
+        isActiveStaging = False
+        isActiveProduction = False
 
-        """Initialize instance of dataset
+        super(Dataset, self).__init__(
+            data=data,
+            datasetDisplayName=datasetDisplayName,
+            uploadDate=uploadDate,
+            versionNumber=versionNumber,
+            datasetType=datasetType,
+            isActiveStaging=isActiveStaging,
+            isActiveProduction=isActiveProduction
+        )
 
+        data.close()
 
-
-        """
-        '''kwargs['is_favorite'] = bool(kwargs['is_favorite'])
-        self.update_kwargs_english(kwargs, 'level1', 'level1_id')
-        self.update_kwargs_english(kwargs, 'level2', 'level2_id')
-        self.update_kwargs_english(kwargs, 'domain', 'domain_id')
-        self.update_kwargs_english(kwargs, 'definition', 'definition_id')
-        self.update_kwargs_english(kwargs, 'label', 'label_id')
-        '''
-
-
-        super(Dataset, self).__init__()
 
     @classmethod
     def get(cls, ID):
--- test/test_dataset.py
+++ test/test_dataset.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """Unit tests for dataset class."""
 import unittest
 
+from pma_api import db, create_app
 from pma_api.models import Dataset
 
 
@@ -14,17 +14,69 @@ class TestDataset(unittest.TestCase):
        python -m test.test_dataset
     """
 
+    def setUp(self):
+        """Set up: (1) Put Flask app in test mode, (2) Create temp DB."""
+        # Continue from here next time
+        # 1 set up the test
+        import tempfile
+        app = create_app()
+        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()  # TODO: Will this work?
+        app.testing = True
+        self.app = app.test_client()
+        with app.app_context():
+            # 2. new dataset object
+            new_dataset = \
+                Dataset(file_path ='/Users/richardnguyen5/Documents/Documents/datalab API dev/pma-api/test/file/api_data-2018.03.19-v29-SAS.xlsx')
+            # 3. write to db
+            db.session.add(new_dataset)
+            db.session.commit()
+
     def test_dataset(self):
         """Create a new entry in 'dataset' table and read data."""
-        # 1. ceate a new Dataset() object
-        dataset = Dataset(file_path ='')
-        print('hi')
+        # 4. read from the db
+        dataset_from_db = ''  # write logic to get from db:
 
-        # 2. write it to DB
-        pass
+        # 5. make assertions
+        self.assertTrue(dataset_from_db.ID != '')
+        self.assertTrue(dataset_from_db.data != '')
+        self.assertTrue(dataset_from_db.datasetDisplayName == 'api_data-2018.03.19-v29-SAS')
+        self.assertTrue(type(dataset_from_db.uploadDate) == "<class 'datetime.datetime'>")
+        self.assertTrue(dataset_from_db.versionNumber == 'v29')
+        self.assertTrue(dataset_from_db.datasetType in ('data', 'metadata', 'full'))
+        self.assertTrue(dataset_from_db.isActiveStaging == False)
+        self.assertTrue(dataset_from_db.isActiveProduction == False)
 
-        # 3. check to make sure it is there (assert something)
-        self.assertTrue(False)  # temp
+    def tearDown(self):
+        """Tear down: (1) Close temp DB."""
+        # 5: remove the stuff we wrote to the db
+        os.close(self.db_fd)
+        os.unlink(app.config['DATABASE'])
+
+# class TestDB(unittest.TestCase):  # TODO: Adapt from tutorial.
+#     """Test database functionality.
+#
+#     Tutorial: http://flask.pocoo.org/docs/0.12/testing/
+#     """
+#
+#     def setUp(self):
+#         """Set up: (1) Put Flask app in test mode, (2) Create temp DB."""
+#         import tempfile
+#         from manage import initdb
+#         self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+#         app.testing = True
+#         self.app = app.test_client()
+#         with app.app_context():
+#             initdb()
+#
+#     def tearDown(self):
+#         """Tear down: (1) Close temp DB."""
+#         os.close(self.db_fd)
+#         os.unlink(app.config['DATABASE'])
+#
+#     def test_empty_db(self):
+#         """Test empty database."""
+#         resp = self.app.get('/')
+#         assert b'No entries here so far' in resp.data
 
 
 if __name__ == '__main__':
